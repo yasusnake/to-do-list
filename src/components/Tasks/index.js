65c7decf762c3268228930d10ded42bef7c8144f
Cristian Mosqueira <cristianmosqueira@Fernandos-Mac-mini.local>
2018-04-19 15:42:26 -0600

    New changes

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -2,30 +2,33 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 
+// Componente
 import Task from '../Task';
 
 class Tasks extends Component {
-
-  render() {
+  render () {
     return (
       <ul className="Tasks">
         {
-          _.map(this.props.data, (task) => <Task 
-                                              key={task.title} 
-                                              dataPrint={task}
-                                              handleDeleteTask={this.props.handleDeleteTask} 
-                                              handleUpdateTask={this.props.handleUpdateTask}
-                                            />)
-        }
+          _.map(this.props.data, (task) => {
+            return (
+              <Task { ...{
+                key              : task.title,
+                dataPrint        : task,
+                handleDeleteTask : this.props.handleDeleteTask,
+                handleUpdateTask : this.props.handleUpdateTask,
+              }}/>
+            );
+          })}
       </ul>
     );
   }
 }
 
 Tasks.propTypes = {
-  data: PropTypes.array,
-  handleUpdateTask: PropTypes.func,
-  handleDeleteTask: PropTypes.func
+  data             : PropTypes.array,
+  handleUpdateTask : PropTypes.func,
+  handleDeleteTask : PropTypes.func,
 };
 
 export default Tasks;
